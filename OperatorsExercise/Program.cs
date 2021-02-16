a4973a057820abb05034498cb00b6b5b83834818
johndward01 <johndward01@yahoo.com>
2021-02-15 20:38:01 -0600

    initial commit

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            a = 17;
+            b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            
+            Console.WriteLine(AreaOfCircle(a)); 
+        }
+
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
